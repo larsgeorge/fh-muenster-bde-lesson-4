67111291c60cabefbf93c50ad1165d4410b65d2c
Lars George <lars@larsgeorge.com>
2013-12-15 16:16:21 +0100

    Adds .gitignore file

---
+++ src/main/java/org/fhmuenster/bde/model/DocumentInfo.java
@@ -0,0 +1,10 @@
+package org.fhmuenster.bde.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * <p/>
+ * User: larsgeorge
+ * Date: 15/12/13 14:32
+ */
+public class DocumentInfo {
+}
---
+++ src/main/java/org/fhmuenster/bde/model/ResultDocument.java
@@ -0,0 +1,10 @@
+package org.fhmuenster.bde.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * <p/>
+ * User: larsgeorge
+ * Date: 15/12/13 12:49
+ */
+public class ResultDocument {
+}
