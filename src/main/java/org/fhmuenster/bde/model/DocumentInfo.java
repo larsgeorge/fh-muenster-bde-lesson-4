7b4ef9fd67f8ac60d4d015883941b2919a17fb1e
Lars George <lars@larsgeorge.com>
2013-12-15 16:21:59 +0100

    Adds extra dir to ignore

--- src/main/java/org/fhmuenster/bde/model/DocumentInfo.java
+++ src/main/java/org/fhmuenster/bde/model/DocumentInfo.java
@@ -1,10 +1,53 @@
 package org.fhmuenster.bde.model;
 
 /**
- * Created by IntelliJ IDEA.
- * <p/>
- * User: larsgeorge
- * Date: 15/12/13 14:32
+ * Holds info on an indexed book.
  */
 public class DocumentInfo {
+  private String shortName;
+  private String title;
+  private String author;
+  private String releaseDate;
+
+  public DocumentInfo() {
+  }
+
+  public DocumentInfo(String shortName, String title, String author, String releaseDate) {
+    this.shortName = shortName;
+    this.title = title;
+    this.author = author;
+    this.releaseDate = releaseDate;
+  }
+
+  public String getShortName() {
+    return shortName;
+  }
+
+  public void setShortName(String shortName) {
+    this.shortName = shortName;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public String getReleaseDate() {
+    return releaseDate;
+  }
+
+  public void setReleaseDate(String releaseDate) {
+    this.releaseDate = releaseDate;
+  }
 }
--- src/main/java/org/fhmuenster/bde/model/ResultDocument.java
+++ src/main/java/org/fhmuenster/bde/model/ResultDocument.java
@@ -1,10 +1,27 @@
 package org.fhmuenster.bde.model;
 
-/**
- * Created by IntelliJ IDEA.
- * <p/>
- * User: larsgeorge
- * Date: 15/12/13 12:49
- */
 public class ResultDocument {
+  private double score = 0.0;
+  private String name = null;
+
+  public ResultDocument(double score, String name) {
+    this.score = score;
+    this.name = name;
+  }
+
+  public double getScore() {
+    return score;
+  }
+
+  public void setScore(double score) {
+    this.score = score;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
