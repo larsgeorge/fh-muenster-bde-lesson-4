ef45faa41371348f22b426681959aa80d2ba0400
Lars George <lars@larsgeorge.com>
2014-12-23 20:42:29 +0100

    Moved Driver up and added FileFormats.

--- src/main/java/org/fhmuenster/bde/Driver.java
+++ src/main/java/org/fhmuenster/bde/Driver.java
@@ -1,7 +1,9 @@
-package org.fhmuenster.bde.mr;
+package org.fhmuenster.bde;
 
 import com.cloudera.cdk.morphline.MorphlineDemo;
+
 import org.apache.hadoop.util.ProgramDriver;
+import org.fhmuenster.bde.fileformats.FileFormats;
 import org.fhmuenster.bde.http.HttpServerMain;
 import org.fhmuenster.bde.mr.tfidf.TFIDFJobController;
 
@@ -15,6 +17,7 @@ public class Driver {
       pgd.addClass("tfidf", TFIDFJobController.class,
         "MapReduce program to compute TF-IDF of input text files.");
       pgd.addClass("testmorphline", MorphlineDemo.class, "Run a morphline locally.");
+      pgd.addClass("fileformats", FileFormats.class, "Create various file formats.");
       pgd.driver(argv);
       // Success
       exitCode = 0;
